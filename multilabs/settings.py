40860bd11921e6b6904ce7fa43a593b5db2b83ea
vogonyo <vogonyo101@gmail.com>
2021-03-19 16:42:37 +0300

    changed dj database url bug

--- multilabs/settings.py
+++ multilabs/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 import django_heroku
-import dj_databse_url
+import dj_database_url
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
