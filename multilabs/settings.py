6030e5f1dc2f0374af4b86bb7d94a88fcdc5b791
vogonyo <vogonyo101@gmail.com>
2021-03-19 13:05:40 +0300

    made changes to whitenoise and static

--- multilabs/settings.py
+++ multilabs/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '44wsg92v+%w_rqo6*l(y^hhyz@x@va8k5+z6*jfv6-@)-ln-&r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://multilabs.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,7 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
